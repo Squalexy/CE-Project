dca747c68e7e4cd7829f527b63a65e49930a916f
Alexy de Almeida <65732412+Squalexy@users.noreply.github.com>
2023-05-12 12:38:54 +0100

    boxplots again

--- statistical_test.py
+++ statistical_test.py
@@ -160,8 +160,8 @@ def main():
         histogram(normal_data, "Normal data", "Num generations", "Fitness", optimization_problem, lb, ub, file_count + 1) 
         file_count += 2"""
         
-        box_plot(deviation_data, f"{optimization_problem} - {lb} to {ub} ")
-        box_plot(normal_data, f"{optimization_problem} - {lb} to {ub} ")
+        box_plot(deviation_data, f"{optimization_problem} - {lb} to {ub} (with deviation)")
+        box_plot(normal_data, f"{optimization_problem} - {lb} to {ub} (without deviation)")
     
     
     box_plot(datas[0:4], "Ackley")
